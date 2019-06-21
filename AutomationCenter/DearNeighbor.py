69463335e0378b8469fa2b9b596997bb0af1cf44
will.rogers <wsrogers@live.unc.edu>
2019-06-21 11:48:29 -0400

    Initial upload

---
+++ AutomationCenter/DearNeighbor.py
@@ -0,0 +1,191 @@
+#from FrontEnd import *
+#import FrontEnd as f
+import re
+from io import StringIO
+from tkinter import *
+import tkinter as tk
+from pdfminer3.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+from pdfminer3.layout import LAParams
+from pdfminer3.pdfpage import PDFPage
+# variables to parse
+txt00 = ""
+pdf00 = ""
+resultwindow = tk
+
+
+def convert(fname):
+    pages = None
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = open(fname, 'rb')
+    # if names and address on odd page; remove not if names and addresses are on even page
+    for pagenumber, page in enumerate(PDFPage.get_pages(infile, pagenums)):
+        if pagenumber % 2:
+            interpreter.process_page(page)
+        else:
+            pass
+
+    # for page in PDFPage.get_pages(infile, pagenums):
+    # interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close()
+    return text
+
+
+def write_to_text():
+    global txt00
+    global pdf00
+
+    text_file = open("Output_1.txt", "w")
+    text = re.sub("\s\s+", " ", convert(pdf00))
+    text_file.write("%s" % text.encode("utf-8"))
+    text_file.close()
+
+
+def pdf_records_file():
+    global txt00
+    global pdf00
+
+    text = convert(pdf00)
+
+    regex = re.compile(r'(\d{1}.*) *\n(.*) *\n([A-Z]{1}[A-Za-z]+|[A-Z]{1})(\w+)?(.*?) *\n(PO Box )?([A-Z]?\d{1,4}?.*?) *\n(\w+)(.*)')
+    txt = regex.findall(text)
+    new_txt = [tuple(filter(None, i))for i in txt]
+    f1 = open('output_pdf.txt', 'w')
+    count = 0
+    list_0 = []
+    list_1 = []
+    list_2 = []
+    list_3 = []
+    for x in new_txt:
+        list_0.append("Record #" + str(count + 1))
+        name = ' '.join(map(str, x[2:4]))
+        address_1 = ' '.join(map(str, x[4:-2]))
+        address_2 = ' '.join(map(str, x[-2:]))
+        list_1.append(address_1.replace("  ", " "))
+        list_2.append(name.replace("  ", " ").strip('\n'))
+        list_3.append(address_2.replace("  ", " ").replace(" ,", ",").strip(' '))
+        count += 1
+    temp_1 = {}
+    for w, x, y, z in zip(list_0, list_2, list_1, list_3):
+        temp_1.setdefault(w, []).append(x)
+        temp_1.setdefault(w, []).append(y)
+        temp_1.setdefault(w, []).append(z)
+    f1.close()
+    return temp_1
+
+
+def records_in_text_file():
+    global txt00
+    global pdf00
+
+    f = open(txt00, 'r')
+    f1 = open('output_text.txt', 'w')
+    count = 0
+    list_0 = []
+    list_1 = []
+    list_2 = []
+    list_3 = []
+    list_4 = []
+    list_5 = []
+    list_6 = []
+    list_7 = []
+
+    for line in f:
+        list_0.append("Record #" + str(count + 1))
+
+        list_1.append('Dear Neighbor')
+
+        if " first = " in line:
+            list_1.append(line.strip('first = ').strip('\n').replace('\"', '').replace("  ", " "))
+
+        if " last = " in line:
+            list_2.append(line.strip('last = ').strip('\n').replace('\"', '').replace("  ", " "))
+
+        if " suffix = " in line:
+            list_3.append(line.strip('suffix = ').strip('\n').replace('\"', '').replace("  ", " "))
+
+        if " address = " in line:
+            list_4.append(line.strip('address = ').strip('\n').replace('\"', '').replace("  ", " "))
+
+        if " city = " in line:
+            list_5.append(line.strip('city = ').strip('\n').replace('\"', '').replace("  ", " ").strip(' '))
+
+        if " st = " in line:
+            list_6.append(line.strip('st = ').strip('\n').replace('\"', '').replace("  ", " ").strip(' '))
+
+        if " zip = " in line:
+            list_7.append(line.strip('zip = ').strip('\n').replace('\"', '').replace("  ", " ").strip(' '))
+        count += 1
+
+    full_name = [a+" "+b for a, b in zip(list_1, list_2)]
+    state_city_zip = [a+", "+b+" "+c for a, b, c in zip(list_5, list_6, list_7)]
+
+    temp_2 = {}
+    for v, w, x, y, z in zip(list_0, full_name, list_3, list_4, state_city_zip):
+        temp_2.setdefault(v, []).append(w)
+        temp_2.setdefault(v, []).append(x)
+        temp_2.setdefault(v, []).append(y)
+        temp_2.setdefault(v, []).append(z)
+
+    for k in temp_2:
+        if temp_2[k][1] == '<not set>':
+            del temp_2[k][1]
+        else:
+            temp_2[k][0] = str(temp_2[k][0] + ", " + temp_2[k][1])
+            del temp_2[k][1]
+
+    return temp_2
+
+
+def main1(textinput, pdfinput):
+   # start_time = time.time()
+    global txt00
+    global pdf00
+    txt00 = textinput
+    pdf00 = pdfinput
+    pdf_dict = pdf_records_file()
+    text_dict = records_in_text_file()
+    different = set()
+    count = 0
+    count1 = 0
+    resultwindow = tk.Tk()
+    resultwindow.title("Results")
+    for key in sorted(set(pdf_dict.keys()) & set(text_dict.keys())):
+        pdf_value = [x.upper() for x in pdf_dict[key]]
+        text_value = [x.upper() for x in text_dict[key]]
+        if pdf_value == text_value:
+            count += 1
+            # print()
+            Label(resultwindow, text=key + " from pdf file " + str(pdf_dict[key])).grid(row=count1, column=0)
+            Label(resultwindow, text="MATCHED", fg="green").grid(row=count1, column=1)
+            Label(resultwindow, text=key + " from text file " + str(text_dict[key])).grid(row=count1, column=2)
+            # print(key, "from pdf file", pdf_dict[key], colored("MATCHED", 'green', attrs=['bold']), key, "from text file", text_dict[key])
+            # print()
+            count1 += 1
+        else:
+            different.add(key)
+            Label(resultwindow, text=key + " from pdf file " + str(pdf_dict[key])).grid(row=count1, column=0)
+            Label(resultwindow, text="DID NOT MATCH", fg="red").grid(row=count1, column=1)
+            Label(resultwindow, text=key + " from text file " + str(text_dict[key])).grid(row=count1, column=2)
+            # print(key, "from pdf file", pdf_dict[key], colored("DID NOT MATCH", 'red', attrs=['bold']), key, "from text file", text_dict[key])
+            # print()
+            count1 +=1
+
+    # print()
+    Label(resultwindow, text="The number of records matched = ").grid(row=count1, column=0)
+    Label(resultwindow, text=count, fg="blue").grid(row=count1 + 1, column=1)
+    # print("The number of records matched = ", colored(count, 'yellow',attrs=['bold']))
+    # print()
+    # print("Total time taken = ", colored(" %s seconds", 'yellow') % (time.time() - start_time))
---
+++ AutomationCenter/FrontEnd.py
@@ -0,0 +1,532 @@
+#!/usr/bin/env python
+# imports
+import re
+from io import StringIO
+from pdfminer3.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+from pdfminer3.layout import LAParams
+from pdfminer3.pdfpage import PDFPage
+from DearNeighbor import main1
+from NameOnPage import main2
+from tkinter import filedialog
+from tkinter import *
+from functools import partial
+import tkinter as tk
+
+
+# Program by Will Rogers
+# Enjoy my spaghetti code
+
+
+# Global Variables
+helpwindow = tk
+bugwindow = tk
+window = tk.Tk()
+v = tk.IntVar()
+quitb = Button
+helpb = Button
+lrcorner = Frame
+llcorner = Frame
+# Menu Buttons/Variables
+direct = None
+email = None
+bies = None
+# Radio Buttons
+DirectButton0 = Radiobutton
+DirectButton1 = Radiobutton
+EmailButton0 = Radiobutton
+EmailButton1 = Radiobutton
+BiesButton0 = Radiobutton
+BiesButton1 = Radiobutton
+# Direct mail automation buttons/Variables
+BoolNeighbor = None
+BoolName = None
+Neighbor0 = Button
+Neighbor1 = Button
+Name0 = Button
+Name1 = Button
+runButton = Button
+boolRun = None
+# PDF/TXT Search Variables/Buttons
+txt0 = ""
+txt1 = ""
+pdf0 = ""
+pdf1 = ""
+Booltxt0 = None
+Booltxt1 = None
+Boolpdf0 = None
+Boolpdf1 = None
+warning = Label
+pdfDis = Label
+txtDis = Label
+Boolwarning = None
+# Functions
+
+
+def submit():
+    if BoolNeighbor:
+        main1(txt0, pdf0)
+    if BoolName:
+        main2(txt1, pdf1)
+
+
+def checker():
+    global Booltxt0
+    global Boolpdf0
+    global BoolNeighbor
+    global BoolName
+    global Boolpdf1
+    global Booltxt1
+    global warning
+    global Boolwarning
+    global pdfDis
+    global txtDis
+    global runButton
+    global boolRun
+    
+    if BoolNeighbor:
+        if Boolpdf0 and Booltxt0:
+            warning.pack_forget()
+            if boolRun:
+                runButton.destroy()
+                boolRun = False
+            runButton = Button(window, text="Run Test", command=partial(submit))
+            runButton.pack()
+            boolRun = True
+        elif not Boolpdf0:
+            if Boolwarning:
+                warning.pack_forget()
+                Boolwarning = False
+            if boolRun:
+                runButton.destroy()
+                boolRun = False
+            warning = Label(window, text="Missing .pdf file, please upload it.", fg="red")
+            warning.pack()
+            Boolwarning = True
+        elif not Booltxt0:
+            if Boolwarning:
+                warning.pack_forget()
+                Boolwarning = False
+            if boolRun:
+                runButton.destroy()
+                boolRun = False
+            warning = Label(window, text="Missing .txt file, please upload it.", fg="red")
+            warning.pack()
+            Boolwarning = True
+    if BoolName:
+        if Boolpdf1 and Booltxt1:
+            warning.pack_forget()
+            if boolRun:
+                runButton.destroy()
+                boolRun = False
+            runButton = Button(window, text="Run Test", command=partial(submit))
+            runButton.pack()
+            boolRun = True
+        elif not Boolpdf1:
+            if Boolwarning:
+                warning.pack_forget()
+                Boolwarning = False
+            if boolRun:
+                runButton.destroy()
+                boolRun = False
+            warning = Label(window, text="Missing .pdf file, please upload it.", fg="red")
+            warning.pack()
+            Boolwarning = True
+
+        elif not Booltxt1:
+            if Boolwarning:
+                warning.pack_forget()
+                Boolwarning = False
+            if boolRun:
+                runButton.destroy()
+                boolRun = False
+            warning = Label(window, text="Missing .txt file, please upload it.", fg="red")
+            warning.pack()
+            Boolwarning = True
+
+
+def pdfsearch0():
+    global pdf0
+    global Boolpdf0
+    global Booltxt0
+    global pdfDis
+    global txtDis
+    global warning
+    global Boolwarning
+    global boolRun
+    
+    window.fileName = filedialog.askopenfilename(initialdir="/", title="Select a pdf File", filetypes=[("pdf files", "*.pdf")])
+    pdf0 = window.fileName
+    if Boolpdf0:
+        warning.pack_forget()
+        pdfDis.destroy()
+        Boolpdf0 = False
+    if boolRun:
+        runButton.destroy()
+        boolRun = False
+    if pdf0 != "":
+        Boolpdf0 = True
+        pdfDis = Label(window, text=".pdf= "+pdf0)
+        pdfDis.pack()
+        checker()
+
+
+def pdfsearch1():
+    global pdf1
+    global Boolpdf1
+    global Booltxt1
+    global pdfDis
+    global txtDis
+    global warning
+    global Boolwarning
+    global boolRun
+
+    window.fileName = filedialog.askopenfilename(initialdir="/", title="Select a pdf File", filetypes=[("pdf files", "*.pdf")])
+    pdf1 = window.fileName
+    if Boolpdf1:
+        warning.pack_forget()
+        pdfDis.destroy()
+        Boolpdf1 = False
+    if boolRun:
+        runButton.destroy()
+        boolRun = False
+    if pdf1 != "":
+        Boolpdf1 = True
+        pdfDis = Label(window, text=".pdf= "+pdf1)
+        pdfDis.pack()
+        checker()
+
+
+def txtsearch0():
+    global txt0
+    global Boolpdf0
+    global Booltxt0
+    global pdfDis
+    global txtDis
+    global warning
+    global Boolwarning
+    global boolRun
+
+    window.fileName = filedialog.askopenfilename(initialdir="/", title="Select a txt File", filetypes=[("txt files", "*.txt")])
+    txt0 = window.fileName
+    if Booltxt0:
+        txtDis.destroy()
+        Booltxt0 = False
+    if boolRun:
+        runButton.destroy()
+        boolRun = False
+    if txt0 != "":
+        warning.pack_forget()
+        Booltxt0 = True
+        txtDis = Label(window, text=".txt= "+txt0)
+        txtDis.pack()
+        checker()
+
+
+def txtsearch1():
+    global txt1
+    global Boolpdf1
+    global Booltxt1
+    global pdfDis
+    global txtDis
+    global warning
+    global Boolwarning
+    global boolRun
+
+    window.fileName = filedialog.askopenfilename(initialdir="/", title="Select a txt File", filetypes=[("txt files", "*.txt")])
+    txt1 = window.fileName
+    if Booltxt1:
+        warning.pack_forget()
+        txtDis.destroy()
+        Booltxt1 = False
+    if txt1 != "":
+        Booltxt1 = True
+        txtDis = Label(window, text=".txt= "+txt1)
+        txtDis.pack()
+        checker()
+
+
+def neighbortest():
+    global BoolNeighbor
+    global Neighbor0
+    global Neighbor1
+    global BoolName
+    global Name0
+    global Name1
+    global boolRun
+
+    if BoolNeighbor:
+        Neighbor0.destroy()
+        Neighbor1.destroy()
+        BoolNeighbor = False
+        if boolRun:
+            runButton.destroy()
+            boolRun = False
+    if BoolName:
+        Name0.destroy()
+        Name1.destroy()
+        BoolName = False
+        if boolRun:
+            runButton.destroy()
+            boolRun = False
+    if not BoolNeighbor:
+        BoolNeighbor = True
+        if boolRun:
+            runButton.destroy()
+            boolRun = False
+        Neighbor0 = Button(window, text="Upload PDF", command=partial(pdfsearch0))
+        Neighbor1 = Button(window, text="Upload TXT", command=partial(txtsearch0))
+        Neighbor0.pack()
+        Neighbor1.pack()
+
+
+def nameonpagetest():
+    global BoolNeighbor
+    global Neighbor0
+    global Neighbor1
+    global BoolName
+    global Name0
+    global Name1
+    global boolRun
+
+    if BoolNeighbor:
+        Neighbor0.destroy()
+        Neighbor1.destroy()
+        BoolNeighbor = False
+        if boolRun:
+            runButton.destroy()
+            boolRun = False
+    if BoolName:
+        Name0.destroy()
+        Name1.destroy()
+        BoolName = False
+        if boolRun:
+            runButton.destroy()
+            boolRun = False
+    if not BoolName:
+        if boolRun:
+            runButton.destroy()
+            boolRun = False
+        Name0 = Button(window, text="Upload PDF", command=partial(pdfsearch1))
+        Name1 = Button(window, text="Upload TXT", command=partial(txtsearch1))
+        Name0.pack()
+        Name1.pack()
+        BoolName = True
+
+
+def directfunction():
+    global direct
+    global email
+    global bies
+    global EmailButton0
+    global EmailButton1
+    global DirectButton0
+    global DirectButton1
+    global BiesButton0
+    global BiesButton1
+    global BoolNeighbor
+    global BoolName
+    global Neighbor0
+    global Neighbor1
+    global Name0
+    global Name1
+
+    if not direct:
+        if bies:
+            BiesButton0.destroy()
+            BiesButton1.destroy()
+            bies = False
+        if email:
+            EmailButton0.destroy()
+            EmailButton1.destroy()
+            email = False
+        DirectButton0 = Radiobutton(window, variable=v, value=0, text="Dear Neighbor", command=partial(neighbortest))
+        DirectButton1 = Radiobutton(window, variable=v, value=1, text="Name On Page", command=partial(nameonpagetest))
+        DirectButton0.pack()
+        DirectButton1.pack()
+        direct = True
+    else:
+        if BoolNeighbor:
+            Neighbor0.destroy()
+            Neighbor1.destroy()
+            BoolNeighbor = False
+        if BoolName:
+            Name0.pack_forget()
+            Name1.pack_forget()
+            BoolName = False
+        direct = False
+        DirectButton0.destroy()
+        DirectButton1.destroy()
+        DirectButton0 = Radiobutton(window, variable=v, value=0, text="Dear Neighbor", command=partial(neighbortest))
+        DirectButton1 = Radiobutton(window, variable=v, value=1, text="Name On Page", command=partial(nameonpagetest))
+        DirectButton0.pack()
+        DirectButton1.pack()
+        direct = True
+
+
+def emailfunction():
+    global direct
+    global email
+    global bies
+    global EmailButton0
+    global EmailButton1
+    global DirectButton0
+    global DirectButton1
+    global BiesButton0
+    global BiesButton1
+    global BoolNeighbor
+    global BoolName
+    global Neighbor0
+    global Neighbor1
+    global Name0
+    global Name1
+
+    if not email:
+        if direct:
+            DirectButton0.destroy()
+            DirectButton1.destroy()
+            if BoolNeighbor:
+                Neighbor0.destroy()
+                Neighbor1.destroy()
+                BoolNeighbor = False
+            if BoolName:
+                Name0.pack_forget()
+                Name1.pack_forget()
+                BoolName = False
+            direct = False
+        if bies:
+            BiesButton0.destroy()
+            BiesButton1.destroy()
+            bies = False
+        EmailButton0 = Radiobutton(window, variable=v, value=2, text="Placeholder 0")
+        EmailButton1 = Radiobutton(window, variable=v, value=3, text="Placeholder 1")
+        EmailButton0.pack()
+        EmailButton1.pack()
+        email = True
+    else:
+        email = False
+        EmailButton0.destroy()
+        EmailButton1.destroy()
+        EmailButton0 = Radiobutton(window, variable=v, value=2, text="Placeholder 0")
+        EmailButton1 = Radiobutton(window, variable=v, value=3, text="Placeholder 1")
+        EmailButton0.pack()
+        EmailButton1.pack()
+        email = True
+
+
+def biesfunction():
+    global direct
+    global email
+    global bies
+    global EmailButton0
+    global EmailButton1
+    global DirectButton0
+    global DirectButton1
+    global BiesButton0
+    global BiesButton1
+    global BoolName
+    global BoolNeighbor
+    global Neighbor0
+    global Neighbor1
+    global Name0
+    global Name1
+
+    if not bies:
+        if email:
+            EmailButton0.destroy()
+            EmailButton1.destroy()
+            email = False
+        if direct:
+            DirectButton0.destroy()
+            DirectButton1.destroy()
+            if BoolNeighbor:
+                Neighbor0.destroy()
+                Neighbor1.destroy()
+                BoolNeighbor = False
+            if BoolName:
+                Name0.pack_forget()
+                Name1.pack_forget()
+                BoolName = False
+            direct = False
+        BiesButton0 = Radiobutton(window, variable=v, value=2, text="Placeholder 2")
+        BiesButton1 = Radiobutton(window, variable=v, value=3, text="Placeholder 3")
+        BiesButton0.pack()
+        BiesButton1.pack()
+        bies = True
+    else:
+        bies = False
+        BiesButton0.destroy()
+        BiesButton1.destroy()
+        BiesButton0 = Radiobutton(window, variable=v, value=2, text="Placeholder 2")
+        BiesButton1 = Radiobutton(window, variable=v, value=3, text="Placeholder 3")
+        BiesButton0.pack()
+        BiesButton1.pack()
+        bies = True
+
+# Main
+
+
+def exitbugs():
+    bugwindow.destroy()
+
+
+def knownbugs():
+    global bugwindow
+    bugwindow = tk.Toplevel(window)
+    Label(bugwindow, text="Below is a list of known bugs",  wraplength=599). grid(row=0, column=0)
+    Label(bugwindow, text="Please email will.rogers@healthgrades.com if you find any more bugs or have a fix for some.", wraplength=599).grid(row=1, column=0)
+    Label(bugwindow, text="• If you need to change the file you are uploading or change the QA, sometimes the text from the text from the previous upload or QA will  still display. if that happens, the program will still work and you just have to ignore the text or previous button.", wraplength=599, justify=LEFT, anchor=W).grid(row=2, column=0)
+    Label(bugwindow, text="• If the Direct Mail QA is too long, the results can be cut off at the bottom of the screen", wraplength=599, justify=LEFT, anchor=W).grid(row=3, column=0)
+    Button(bugwindow, text="Exit", command=partial(exitbugs)).grid(row=4, column=0)
+
+
+def exitprogram():
+    helpwindow.destroy()
+
+
+def helpme():
+    global helpwindow
+    helpwindow = tk.Toplevel(window)
+    helpwindow.title("Help Menu")
+    Label(helpwindow, text="Welcome to the Healthgrades AutomationCenter 0.2", wraplength=599, justify=LEFT, anchor=W).grid(row=0, column=0)
+    Label(helpwindow, text="This program was created by William Rogers, a quality assurance intern.", wraplength=599, justify=LEFT, anchor=W).grid(row=1, column=0)
+    Label(helpwindow, text=" ", wraplength=599, justify=LEFT, anchor=W).grid(row=2, column=0)
+    Label(helpwindow, text="• While this intern has never made a GUI before, has never used Python and barely knows how this works, he will do his best to walk you through it in this help section.", wraplength=599, justify=LEFT, anchor=W).grid(row=3, column=0)
+    Label(helpwindow, text="• To get started, use the dropdown menu to select what QA type you are doing. Currently, only automation for DirectMail is available.", wraplength=599, justify=LEFT, anchor=W).grid(row=5, column=0)
+    Label(helpwindow, text="• This will then present you with the buttons to select what autmation you need for this QA. If the button appears clicked when you first see the option you still have to click it again.", wraplength=599, justify=LEFT, anchor=W).grid(row=7, column=0)
+    Label(helpwindow, text="• You will then be presented with buttons to upload the files needed for this QA automation. Please upload all files asked for in order to continue.", wraplength=599, justify=LEFT, anchor=W).grid(row=9, column=0)
+    Label(helpwindow, text="• All that is left is to hit the submit button and the automation will do the work for you! If you are having troble reading the results, be sure to put the window in full screen.", wraplength=599, justify=LEFT, anchor=W).grid(row=10, column=0)
+    Label(helpwindow, text=" ", wraplength=599, justify=LEFT, anchor=W).grid(row=11, column=0)
+    Label(helpwindow, text="Thank you for using my program :)", wraplength=599, justify=CENTER, anchor=N).grid(row=12, column=0)
+    Button(helpwindow, text="Known Bugs", command=partial(knownbugs)).grid(row=13, column=0)
+    Button(helpwindow, text="Exit", command=partial(exitprogram)).grid(row=14, column=0)
+
+
+def quitprogram():
+    window.destroy()
+
+
+window.geometry("600x450")
+window.title("AutomationCenter for HealthGrades 0.2")
+Label(window, text="AutomationCenter").pack()
+Label(window, text="By Will Rogers").pack()
+Label(window, text="Select the Automation you would Like:").pack()
+
+mb = Menubutton(window, text="QA type")
+mb.menu = Menu(mb)
+mb["menu"] = mb.menu
+mb.menu.add_command(label="DirectMail", command=partial(directfunction))
+mb.menu.add_command(label="Email", command=partial(emailfunction))
+mb.menu.add_command(label="BIES", command=partial(biesfunction))
+mb.pack()
+
+lrcorner = Frame(padx=0, pady=0)
+llcorner = Frame(padx=0, pady=0)
+lrcorner.pack(side="left", expand=True)
+llcorner.pack(side="right", expand=True)
+quitb = Button(llcorner, text="Quit", anchor=SW, command=partial(quitprogram))
+helpb = Button(lrcorner, text="Help", anchor=SE, command=partial(helpme))
+quitb.pack()
+helpb.pack()
+
+window.mainloop()
