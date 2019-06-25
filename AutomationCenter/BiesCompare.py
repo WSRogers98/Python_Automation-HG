715a5f9e247778cbf813ace5aed8ad5ca3668c27
will.rogers <wsrogers@live.unc.edu>
2019-06-25 09:32:52 -0400

    Version Update

--- AutomationCenter/BiesCompare.py
+++ AutomationCenter/BiesCompare.py
@@ -3,3 +3,12 @@ import tkinter as tk
 from pandas import *
 import re
 import math
+from beautifulsoup4 import *
+from htmltopdf import *
+from io import StringIO
+from pdfminer3.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+from pdfminer3.layout import LAParams
+from pdfminer3.pdfpage import PDFPage
+
+
